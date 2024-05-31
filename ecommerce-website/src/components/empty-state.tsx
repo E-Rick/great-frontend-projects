ad968f7656aa0b1ed40a8b0ed56728b5d790c3d5
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-05-30 23:12:44 -0400

    refactor(empty-state): adjust spacing

--- ecommerce-website/src/components/empty-state.tsx
+++ ecommerce-website/src/components/empty-state.tsx
@@ -12,13 +12,13 @@ export const EmptyState = ({
 	icon,
 }: EmptyStateProps) => {
 	return (
-		<section className='m-auto flex flex-col w-full h-full gap-5 justify-center items-center'>
+		<div className='flex flex-col w-full gap-5 justify-center items-center'>
 			<div className='shadow bg-white p-3 rounded-full'>{icon}</div>
 			<div className='flex flex-col items-center gap-2'>
 				<h2 className='text-xl font-medium text-primary'>{header}</h2>
 				<p className='text-base text-primary'>{subheader}</p>
 			</div>
 			<div>{actions}</div>
-		</section>
+		</div>
 	)
 }
