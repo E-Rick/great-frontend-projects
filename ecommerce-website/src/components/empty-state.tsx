64a82f476de4a76925e00c2c3f8774a09b881d0e
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-05-30 22:55:04 -0400

    feat(components): add empty state component

---
+++ ecommerce-website/src/components/empty-state.tsx
@@ -0,0 +1,24 @@
+type EmptyStateProps = {
+	header: string
+	subheader: string
+	actions?: React.ReactNode
+	icon?: React.ReactNode
+}
+
+export const EmptyState = ({
+	header,
+	subheader,
+	actions,
+	icon,
+}: EmptyStateProps) => {
+	return (
+		<section className='m-auto flex flex-col w-full h-full gap-5 justify-center items-center'>
+			<div className='shadow bg-white p-3 rounded-full'>{icon}</div>
+			<div className='flex flex-col items-center gap-2'>
+				<h2 className='text-xl font-medium text-primary'>{header}</h2>
+				<p className='text-base text-primary'>{subheader}</p>
+			</div>
+			<div>{actions}</div>
+		</section>
+	)
+}
