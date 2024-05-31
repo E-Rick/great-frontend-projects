dbdcd337ccfdf1a7bcff25a737e77c0aee0bf240
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-05-31 15:31:41 -0400

    feat(utils): add convertSize function

--- ecommerce-website/src/lib/utils.ts
+++ ecommerce-website/src/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+
+export function convertSize(size: string) {
+  return size === 'xs'
+    ? 'Extra Small'
+    : size === 's'
+      ? 'Small'
+      : size === 'm'
+        ? 'Medium'
+        : size === 'l'
+          ? 'Large'
+          : size === 'xl'
+            ? 'Extra Large'
+            : size === 'xxl'
+              ? 'Extra Extra Large'
+              : size === 'xxxl'
+                ? 'Extra Extra Extra Large'
+                : size
+}
