e71e6596b0885e9e82e3bee5e44cca741ac9fc18
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-07-22 00:26:50 -0400

    feat(nav): add mobile nav sheet

--- ecommerce-website/src/components/ui/nav/nav.tsx
+++ ecommerce-website/src/components/ui/nav/nav.tsx
@@ -1,9 +1,16 @@
 import { MaxWidthWrapper } from "@/components/ui/max-width-wrapper";
+import {
+  Sheet,
+  SheetTrigger,
+  SheetContent,
+  SheetHeader,
+  SheetClose,
+} from "@/components/ui/sheet";
 import WordMark from "@/components/ui/word-mark";
 import { cn } from "@/lib/utils";
 import { cva } from "class-variance-authority";
 import Link from "next/link";
-import { RiShoppingBag3Line } from "react-icons/ri";
+import { RiShoppingBag3Line, RiMenuFill, RiCloseLine } from "react-icons/ri";
 
 export const navItems = [
   { name: "Shop all", href: "/" },
@@ -13,7 +20,7 @@ export const navItems = [
   },
 ];
 
-const navItemStyles = cva([
+export const navItemStyles = cva([
   "text-secondary font-medium rounded-[4px] px-0.5",
   "focus:outline-none focus:ring focus:ring-nav-item/15",
   "hover:text-primary disabled:text-disabled transition-colors",
@@ -25,20 +32,61 @@ export function Nav() {
       <MaxWidthWrapper className="relative">
         <nav className="flex w-full items-center gap-[103px]">
           <WordMark />
-          <ul className="flex grow gap-8">
+          <ul className="flex gap-8 grow">
             {navItems.map((navItem) => (
               <li key={navItem.name}>
-                <Link href={navItem.href} className={navItemStyles()}>
+                <Link
+                  href={navItem.href}
+                  className={cn(navItemStyles(), "hidden lg:inline-flex")}
+                >
                   {navItem.name}
                 </Link>
               </li>
             ))}
           </ul>
-          <Link href="/cart" className={cn(navItemStyles(), "p-0")}>
-            <RiShoppingBag3Line size={24} />
-          </Link>
+
+          <div className="inline-flex gap-4">
+            <Link href="/cart" className={cn(navItemStyles(), "p-0")}>
+              <RiShoppingBag3Line size={24} />
+            </Link>
+            <NavMobileMenu />
+          </div>
         </nav>
       </MaxWidthWrapper>
     </header>
+  );
+}
+
+function NavMobileMenu() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <button className={cn(navItemStyles(), "lg:hidden")}>
+          <RiMenuFill size={20} />
+        </button>
+      </SheetTrigger>
+      <SheetContent
+        side="left"
+        className="flex flex-col gap-6 bg-white"
+        hideClose
+      >
+        <SheetHeader className="flex-row items-center justify-between w-full space-y-0">
+          <WordMark />
+          <SheetClose className={cn(navItemStyles(), "")}>
+            <RiCloseLine size={20} />
+            <span className="sr-only">Close</span>
+          </SheetClose>
+        </SheetHeader>
+        <ul className="flex flex-col">
+          {navItems.map((navItem) => (
+            <li key={navItem.name} className="gap-3 px-3 py-2">
+              <Link href={navItem.href} className={cn(navItemStyles(), "")}>
+                {navItem.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </SheetContent>
+    </Sheet>
   );
 }
