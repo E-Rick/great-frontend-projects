2e5ba77963999cc1e049feac73ccb08bf1a70636
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-07-21 21:53:16 -0400

    build(tw): add more style guide colors

--- ecommerce-website/tailwind.config.ts
+++ ecommerce-website/tailwind.config.ts
@@ -10,10 +10,32 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        'primary-invert': colors.white,
         primary: colors.neutral[900],
+        'primary-hover': colors.neutral[950],
+        'primary-invert': colors.white,
         secondary: colors.neutral[600],
+        tertiary: colors.neutral[500],
         brand: colors.indigo[700],
+        disabled: colors.neutral[400],
+        error: colors.red[600],
+        'error-emphasize': colors.red[800],
+        success: colors.green[700],
+        warning: colors.amber[700],
+        'primary-bg': colors.white,
+        'primary-inverted-bg': colors.neutral[950],
+        'primary-hover-bg': colors.neutral[50],
+        'secondary-bg': colors.gray[200],
+        'secondary-hover-bg': colors.gray[300],
+        'tertiary-bg': colors.neutral[50],
+        'disabled-bg': colors.neutral[100],
+        'disabled-emphasize': colors.gray[100],
+        'line-primary': colors.neutral[200],
+        'line-secondary': colors.gray[400],
+        'line-success': colors.green[200],
+        'brand-solid': colors.indigo[600],
+        'brand-subtle': colors.indigo[200],
+        'error-subtle': colors.red[200],
+        'warning-subtle': colors.amber[200]
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
