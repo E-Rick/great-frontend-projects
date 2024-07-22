d5abbe75716e81b6b8d1105808662dad7bb90d52
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-07-21 21:42:52 -0400

    feat: add max-width-wrapper comp

---
+++ ecommerce-website/src/components/ui/max-width-wrapper.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@/lib/utils'
+import { ReactNode } from 'react'
+
+export function MaxWidthWrapper({
+	className,
+	children,
+}: {
+	className?: string
+	children: ReactNode
+}) {
+	return (
+		<div
+			className={cn('mx-auto w-full max-w-screen-xl px-4 lg:px-8', className)}
+		>
+			{children}
+		</div>
+	)
+}
