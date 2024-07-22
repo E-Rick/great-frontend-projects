ca4960119f4059d364d8845c220896029a0a08cd
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-07-22 14:56:21 -0400

    feat(hooks): add use-product-query hook

---
+++ ecommerce-website/src/hooks/use-product-query.ts
@@ -0,0 +1,24 @@
+import { ProductDetail } from "@/lib/types";
+import { useQuery } from "@tanstack/react-query";
+import axios from 'axios';
+
+export const fetchProductById = async ({
+  queryKey,
+}): Promise<ProductDetail> => {
+  const productId = queryKey[1];
+  const { data } = await axios.get(
+    `https://www.greatfrontend.com/api/projects/challenges/e-commerce/products/${productId}`,
+  );
+  return data;
+};
+
+
+export const useProductQuery = <TData = ProductDetail,>(
+  productId: string,
+  select?: (data: ProductDetail) => TData,
+) =>
+  useQuery({
+    queryKey: ["product", productId],
+    queryFn: fetchProductById,
+    select,
+  });
