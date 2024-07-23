479949ffe25b27f986c4b5c33eb6022a62cdc6c2
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-07-23 17:36:56 -0400

    feat(color-selector): add out of stock state and check color inventory

--- ecommerce-website/src/app/product/[id]/ui/color-selector.tsx
+++ ecommerce-website/src/app/product/[id]/ui/color-selector.tsx
@@ -3,7 +3,9 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { useProductData } from "@/hooks/use-product-query";
 import { cn } from "@/lib/utils";
+import { useEffect, useMemo } from "react";
 import { RiCheckFill } from "react-icons/ri";
+import { useProductInventoryByColor } from "../../../../hooks/use-product-query";
 
 const useProductColors = (productId: string) =>
   useProductData(productId, (data) => data.colors);
@@ -13,9 +15,9 @@ export function ColorSelector({ productId }: { productId: string }) {
   const isColorNullOrUndefined = colors === null || colors === undefined;
   const { activeColor, setActiveColor } = useProductContext();
 
-  // const [activeColor, setActiveColor] = useState(
-  //   isColorNullOrUndefined ? null : colors[0],
-  // );
+  useEffect(() => {
+    if (colors !== null && colors !== undefined) setActiveColor(colors[0]);
+  }, [colors, setActiveColor]);
 
   if (isColorNullOrUndefined) return null;
 
@@ -28,28 +30,36 @@ export function ColorSelector({ productId }: { productId: string }) {
       <Label>Available Colors</Label>
 
       <div className="flex flex-wrap w-full gap-4">
-        {colors.map((color) => (
-          <div key={color} className="p-[9.33px]">
-            <Selector
-              key={color}
-              color={color}
-              isActive={color === activeColor}
-              onClick={handleSelectorClick}
-            />
-          </div>
-        ))}
+        {colors.map((color) => {
+          // Check the stock for that color
+
+          return (
+            <div key={color} className="p-[9.33px]">
+              <Selector
+                key={color}
+                color={color}
+                isActive={color === activeColor}
+                onClick={handleSelectorClick}
+                productId={productId}
+              />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 }
 
 type SelectorProps = {
+  productId: string;
   isActive: boolean;
   onClick: (color: string) => void;
   color: string;
 };
 
-function Selector({ isActive, onClick, color }: SelectorProps) {
+function Selector({ isActive, onClick, color, productId }: SelectorProps) {
+  const colorInventory = useProductInventoryByColor(productId, color);
+
   const colorVariants = {
     beige: "bg-beige-500",
     black: "bg-black",
@@ -65,6 +75,11 @@ function Selector({ isActive, onClick, color }: SelectorProps) {
 
   const isWhite = color === "white";
 
+  const isColorOutOfStock = useMemo(
+    () => colorInventory?.every((item) => item.stock === 0),
+    [colorInventory],
+  );
+
   return (
     <Button
       variant="ghost"
@@ -75,9 +90,29 @@ function Selector({ isActive, onClick, color }: SelectorProps) {
       )}
       onClick={() => onClick(color)}
     >
-      {isActive ? (
+      {isColorOutOfStock ? (
+        <Line />
+      ) : isActive ? (
         <RiCheckFill size={28} className="text-primary-invert" />
       ) : null}
     </Button>
+  );
+}
+
+function Line() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="34"
+      height="34"
+      fill="none"
+      viewBox="0 0 34 34"
+    >
+      <path
+        fill="#525252"
+        d="M32.4 0.2H34.5V45H32.4z"
+        transform="rotate(45 32.4 .2)"
+      ></path>
+    </svg>
   );
 }
--- ecommerce-website/src/hooks/use-product-query.ts
+++ ecommerce-website/src/hooks/use-product-query.ts
@@ -30,3 +30,36 @@ export const useProductData = <TData = ProductDetail,>(
 ) => {
   return useProductQuery(productId, select).data
 }
+
+export const useProductReviewCount = (productId: string) =>
+  useProductData(productId, (data) => data.reviews);
+export const useProductRating = (productId: string) =>
+  useProductData(productId, (data) => data.rating);
+
+
+
+export const useProductInventory = (
+  productId: string,
+  color?: string | null,
+  size?: string | number | null,
+) =>
+  useProductData(productId, (data) => {
+    if (size === null && color === null) {
+      // check if inventory isn't undefined
+      if (data.inventory !== undefined)
+        return data.inventory[0]
+    } else {
+      return data.inventory?.find((inventory) => {
+        return inventory.color === color && inventory.size === size;
+      });
+    }
+  });
+
+export const useProductInventoryByColor = (
+  productId: string,
+  color?: string | null,
+) =>
+  useProductData(productId, (data) => {
+    const inventory = data.inventory
+    return inventory?.filter(item => item.color === color)
+  })
