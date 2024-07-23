17205022bd3246caaaf8991a1c596edcedda399e
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-07-23 16:32:34 -0400

    feat(color-selector): handle white color edge case

--- ecommerce-website/src/app/product/[id]/ui/color-selector.tsx
+++ ecommerce-website/src/app/product/[id]/ui/color-selector.tsx
@@ -1,8 +1,8 @@
+import useProductContext from "@/app/product/[id]/product-context";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { useProductData } from "@/hooks/use-product-query";
 import { cn } from "@/lib/utils";
-import { useState } from "react";
 import { RiCheckFill } from "react-icons/ri";
 
 const useProductColors = (productId: string) =>
@@ -11,10 +11,11 @@ const useProductColors = (productId: string) =>
 export function ColorSelector({ productId }: { productId: string }) {
   const colors = useProductColors(productId);
   const isColorNullOrUndefined = colors === null || colors === undefined;
+  const { activeColor, setActiveColor } = useProductContext();
 
-  const [activeColor, setActiveColor] = useState(
-    isColorNullOrUndefined ? null : colors[0],
-  );
+  // const [activeColor, setActiveColor] = useState(
+  //   isColorNullOrUndefined ? null : colors[0],
+  // );
 
   if (isColorNullOrUndefined) return null;
 
@@ -23,10 +24,10 @@ export function ColorSelector({ productId }: { productId: string }) {
   };
 
   return (
-    <div className="flex w-full flex-col gap-4">
+    <div className="flex flex-col w-full gap-4">
       <Label>Available Colors</Label>
 
-      <div className="flex w-full flex-wrap gap-4">
+      <div className="flex flex-wrap w-full gap-4">
         {colors.map((color) => (
           <div key={color} className="p-[9.33px]">
             <Selector
@@ -56,18 +57,21 @@ function Selector({ isActive, onClick, color }: SelectorProps) {
     pink: "bg-pink-500",
     yellow: "bg-yellow-500",
     green: "bg-emerald-500",
-    white: "bg-white",
+    white: "bg-white border-black border-[1px]",
     brown: "bg-brown-500",
     blue: "bg-blue-500",
     red: "bg-red-500",
   };
+
+  const isWhite = color === "white";
 
   return (
     <Button
       variant="ghost"
       className={cn(
         `h-[38px] w-[38px] rounded-full p-0 ${colorVariants[color]}`,
-        isActive && "border-[2.33px] border-white ring-1 ring-brand",
+        isActive && "ring-1 ring-brand",
+        isActive && !isWhite && "border-[2.33px] border-white",
       )}
       onClick={() => onClick(color)}
     >
