8db5d809ec8ce7a90c5cbbf1e82368060eb97047
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-08-01 15:29:27 -0400

    feat(review-rating): add colored progress bars
    * add percentage calculations

--- ecommerce-website/src/components/product/review-rating.tsx
+++ ecommerce-website/src/components/product/review-rating.tsx
@@ -8,8 +8,8 @@ import {
 } from "@/components/ui/dialog";
 import { Progress } from "@/components/ui/progress";
 import { StarRating } from "@/components/ui/star-rating";
-import { useProductReviewData } from "@/hooks/use-product-reviews";
 import { RiChatSmile3Line } from "react-icons/ri";
+import { useProductReviewsQuery } from "../../hooks/use-product-reviews";
 
 export function ReviewRating({
   reviewCount,
@@ -56,6 +56,11 @@ export function ReviewRating({
                 {roundedRating}
               </span>
               <StarRating rating={rating} />
+              {hasReviews && (
+                <span className="text-sm font-normal text-neutral-600">
+                  Based on {reviewCount} reviews
+                </span>
+              )}
             </div>
           </div>
           <RatingValues productId={productId} />
@@ -79,27 +84,78 @@ export function ReviewRating({
   );
 }
 
-const LABELS = ["Excellent", "Good", "Average", "Below Average", "Poor"];
+const RATINGS = [
+  {
+    label: "Excellent",
+    color: "bg-green-600",
+    rating: 5,
+    countsIndex: 4,
+  },
+  {
+    label: "Good",
+    color: "bg-green-500",
+    rating: 4,
+    countsIndex: 3,
+  },
+  {
+    label: "Average",
+    color: "bg-yellow-300",
+    rating: 3,
+    countsIndex: 2,
+  },
+  {
+    label: "Below Average",
+    color: "bg-yellow-500",
+    rating: 2,
+    countsIndex: 1,
+  },
+  {
+    label: "Poor",
+    color: "bg-red-600",
+    rating: 1,
+    countsIndex: 0,
+  },
+];
 
 function RatingValues({ productId }: { productId: string }) {
-  const data = useProductReviewData(productId);
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useProductReviewsQuery(productId, "5");
+
   console.log(`🚀 ---------------🚀`);
   console.log(`🚀 ~ data:`, data);
   console.log(`🚀 ---------------🚀`);
   return (
     <div className="flex w-full flex-col gap-3 py-4">
-      {Array.from({ length: 5 }).map((_, i) => {
+      {RATINGS.map((ratingValue) => {
+        const aggregate = data?.pages[0]?.aggregate;
+        const count = aggregate?.counts?.[ratingValue.countsIndex]?.count ?? 0;
+        const total = aggregate?.total ?? 1;
+        const ratio = count / total;
+        const percentage = Math.round(isNaN(ratio) ? 0 : ratio * 100);
         return (
-          <div key={i} className="flex items-center gap-2">
+          <div key={ratingValue.color} className="flex items-center gap-2">
             <Button
               variant="link"
               size="lg"
               className="min-w-[120px] text-disabled"
             >
-              <span className="mr-auto">{LABELS[i]}</span>
+              <span className="mr-auto">{ratingValue.label}</span>
             </Button>
-            <Progress className="w-full" />
-            <span className="min-w-[42px] text-right">0%</span>
+            <Progress
+              value={percentage}
+              className={`w-full`}
+              indicatorColor={ratingValue.color}
+            />
+            <span className="min-w-[42px] text-right text-neutral-600">
+              {percentage}%
+            </span>
           </div>
         );
       })}
