00313bf9369f6f0d61350b2c80d4821775bf574d
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-08-01 15:28:54 -0400

    feat(use-product-reviews): change to infinite query

--- ecommerce-website/src/hooks/use-product-reviews.ts
+++ ecommerce-website/src/hooks/use-product-reviews.ts
@@ -1,31 +1,40 @@
 import { ProductReview } from "@/lib/product-review-types";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import axios from 'axios';
 
 
 export const fetchProductReviewsById = async ({
   queryKey,
+  pageParam
 }): Promise<ProductReview> => {
   const productId = queryKey[1];
-  const { data } = await axios.get(
-    `https://www.greatfrontend.com/api/projects/challenges/e-commerce/products/${productId}/reviews`,
-  );
+  const rating = queryKey[2]
+  const url = new URL(`https://www.greatfrontend.com/api/projects/challenges/e-commerce/products/${productId}/reviews`)
+  if (rating !== undefined) {
+    url.searchParams.append('rating', rating)
+  }
+  const { data } = await axios.get(url.toString());
   return data;
 };
 
 export const useProductReviewsQuery = <TData = ProductReview>(
   productId: string,
-  select?: (data: ProductReview) => TData,
+  rating?: string
 ) =>
-  useQuery({
-    queryKey: ["review", productId],
+  useInfiniteQuery({
+    queryKey: ["reviews", productId, rating],
     queryFn: fetchProductReviewsById,
-    select,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages, lastPageParam) => {
+      if (!lastPage.pagination.has_more) {
+        return undefined
+      }
+      return lastPageParam + 1
+    },
   });
 
 export const useProductReviewData = <TData = ProductReview,>(
   productId: string,
-  select?: (data: ProductReview) => TData,
 ) => {
-  return useProductReviewsQuery(productId, select).data
+  return useProductReviewsQuery(productId).data
 }
