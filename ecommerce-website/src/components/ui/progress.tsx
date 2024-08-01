858b4d8dd4597241aeed868dd68c7041943b4167
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-08-01 15:28:13 -0400

    feat(progress): add indicator color

--- ecommerce-website/src/components/ui/progress.tsx
+++ ecommerce-website/src/components/ui/progress.tsx
@@ -5,20 +5,26 @@ import * as React from "react";
 
 import { cn } from "@/lib/utils";
 
+type CustomProgressProps = React.ComponentPropsWithoutRef<
+  typeof ProgressPrimitive.Root
+> & {
+  indicatorColor: string;
+};
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+  CustomProgressProps
+>(({ className, value, indicatorColor, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
+      "relative h-2 w-full overflow-hidden rounded-full bg-gray-200",
       className,
     )}
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
+      className={`h-full w-full flex-1 rounded-full bg-primary transition-all ${indicatorColor}`}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
