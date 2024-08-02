244e358d096cea98798b53d34be8f7164c98e3da
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-08-02 14:50:11 -0400

    style(avatar): match fallback to designs

--- ecommerce-website/src/components/ui/avatar.tsx
+++ ecommerce-website/src/components/ui/avatar.tsx
@@ -39,7 +39,7 @@ const AvatarFallback = React.forwardRef<
   <AvatarPrimitive.Fallback
     ref={ref}
     className={cn(
-      "flex h-full w-full items-center justify-center rounded-full bg-neutral-200",
+      "flex h-full w-full items-center justify-center rounded-full bg-gray-200 text-xl font-medium text-neutral-600",
       className,
     )}
     {...props}
