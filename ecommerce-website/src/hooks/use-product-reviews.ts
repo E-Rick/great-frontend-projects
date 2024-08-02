f4f5cb063d695c5bf655edc592d74e1c6f276bbd
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-08-01 22:00:38 -0400

    fix(use-product-reviews): handle null rating param edge case
    * add type docs

--- ecommerce-website/src/hooks/use-product-reviews.ts
+++ ecommerce-website/src/hooks/use-product-reviews.ts
@@ -34,13 +34,23 @@ export const useProductReviewsQuery = <TData = InfiniteData<ProductReview>>(
     select
   });
 
-// Returns the review count for a filtered product review
+/**
+ * Selects the review count for a filtered product review.
+ * @param productId - string
+ * @param rating - string | null
+ * @returns { filteredReviewCount } Returns the review count for a filtered product review. Default to aggregate's total review count if `rating` param is null.
+ */
 export const useFilteredProductReviewCount = <TData = InfiniteData<ProductReview>>(
   productId: string,
   rating?: string | null
 ) => {
   return useProductReviewsQuery(productId, rating, (data) => {
-    const filteredReviewCount = data.pages[0]?.aggregate.counts[Number(rating) - 1]?.count
+    let countIndex = Number(rating) - 1 // Number() converts null to 0
+    let filteredReviewCount = data.pages[0]?.aggregate.counts[countIndex]?.count
+    // If count index is -1 then the rating passed in was null, so use total review count
+    if (countIndex === -1) {
+      filteredReviewCount = data.pages[0]?.aggregate.total
+    }
     return {
       filteredReviewCount: filteredReviewCount !== undefined ? filteredReviewCount : 0
     }
