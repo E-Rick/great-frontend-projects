2c1e07e4d4bd54b63adcaffcb7325768a10a8c61
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-08-02 14:05:10 -0400

    feat(use-product-reviews): improve typeguards on params

--- ecommerce-website/src/hooks/use-product-reviews.ts
+++ ecommerce-website/src/hooks/use-product-reviews.ts
@@ -1,8 +1,11 @@
 import { ProductReview } from "@/lib/product-review-types";
+import { isNotNullish } from "@/lib/typeguards";
 import { InfiniteData, useInfiniteQuery } from "@tanstack/react-query";
 import axios from 'axios';
 
-const defaultQueryParams = { pageSize: 12, page: 1 }
+export const defaultQueryParams = { pageSize: 12, page: 1 }
+
+const PRODUCTS_BASE_URL = 'https://www.greatfrontend.com/api/projects/challenges/e-commerce/products'
 
 export const fetchProductReviewsById = async ({
   queryKey,
@@ -11,12 +14,15 @@ export const fetchProductReviewsById = async ({
   const productId = queryKey[1];
   const pageSize = queryKey[2] ?? defaultQueryParams.pageSize
   const rating = queryKey[3]
-  const url = new URL(`https://www.greatfrontend.com/api/projects/challenges/e-commerce/products/${productId}/reviews`)
+  const url = new URL(`${PRODUCTS_BASE_URL}/${productId}/reviews`)
+
   url.searchParams.append("per_page", pageSize)
-  if (pageParam !== undefined) {
+
+  if (isNotNullish(pageParam)) {
     url.searchParams.append("page", pageParam)
   }
-  if (rating !== undefined) {
+
+  if (isNotNullish(rating)) {
     url.searchParams.append('rating', rating)
   }
 
@@ -38,7 +44,7 @@ export const useProductReviewsQuery = <TData = InfiniteData<ProductReview>>(
       if (!lastPage.pagination.has_more) {
         return undefined
       }
-      return lastPageParam + 1
+      return lastPage.pagination.page + 1
     },
     select
   });
