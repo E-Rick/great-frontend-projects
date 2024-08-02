5d4261144e1af809d0c5d609ba377d07d1f88f57
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-08-01 23:22:17 -0400

    style(review-rating): fix padding on dialog footer

--- ecommerce-website/src/components/product/review-rating.tsx
+++ ecommerce-website/src/components/product/review-rating.tsx
@@ -85,7 +85,7 @@ export function ReviewRating({
           </div>
         )}
       </div>
-      <DialogContent className="flex h-[calc(100%_-96px)] w-full max-w-[calc(100%_-_24px)] flex-col gap-8 overflow-hidden overflow-y-auto rounded-lg px-0 pt-[72px] md:max-w-[522px] lg:h-[calc(100%_-160px)] lg:max-w-[1008px] lg:flex-row">
+      <DialogContent className="flex h-[calc(100%_-96px)] w-full max-w-[calc(100%_-_24px)] flex-col gap-8 overflow-hidden overflow-y-auto rounded-lg px-0 pt-[72px] md:max-w-[522px] md:pb-0 lg:h-[calc(100%_-160px)] lg:max-w-[1008px] lg:flex-row">
         <div className="left-container flex shrink-0 flex-col gap-6 px-3">
           <div className="heading flex flex-col gap-2 lg:min-w-[312px]">
             <DialogTitle>Overall Rating</DialogTitle>
@@ -138,7 +138,7 @@ export function ReviewRating({
                   );
                 })}
               </div>
-              <DialogFooter>
+              <DialogFooter className="py-6">
                 {hasNextPage && (
                   <Button
                     variant="secondary"
@@ -155,7 +155,7 @@ export function ReviewRating({
             </div>
           ) : (
             <EmptyState
-              className="h-fit p-6"
+              className="h-fit p-6 md:h-full"
               header="No reviews yet!"
               subheader="Be the first to review this product"
               icon={<RiChatSmile3Line size={24} className="text-brand" />}
