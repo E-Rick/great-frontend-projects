0aedc63e08546ca49dbc7ae6c5d661dc3b3c756c
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-08-02 20:01:09 -0400

    style(review-list): match figma designs

--- ecommerce-website/src/components/review/review-list.tsx
+++ ecommerce-website/src/components/review/review-list.tsx
@@ -19,10 +19,10 @@ export function ReviewList({ data }: { data: DataEntity[] }) {
               </Avatar>
               <div className="details flex flex-auto flex-col items-start gap-1">
                 <div className="title flex w-full items-center justify-between">
-                  <span className="font-semibold text-primary">
+                  <span className="text-base font-semibold text-primary">
                     {user.name}
                   </span>
-                  <span className="text-sm font-normal text-neutral-600">
+                  <span className="text-xs font-normal text-neutral-600">
                     {formatDate(review.created_at)}
                   </span>
                 </div>
