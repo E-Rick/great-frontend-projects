52b41c5af35d8ad313b8a3a0327e25c9cf3e8c22
Erick Martinez Jr <erickmartinezjunior@gmail.com>
2024-08-03 18:45:39 -0400

    style(badge): adjust responsive font styles

--- ecommerce-website/src/components/ui/badge.tsx
+++ ecommerce-website/src/components/ui/badge.tsx
@@ -4,7 +4,10 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-full w-fit px-2.5 py-1 text-xs font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-brand focus:ring-offset-2",
+  [
+    "inline-flex items-center rounded-full w-fit px-2.5 py-1 text-xs lg:font-normal lg:text-sm font-medium",
+    " transition-colors focus:outline-none focus:ring-2 focus:ring-brand focus:ring-offset-2",
+  ],
   {
     variants: {
       variant: {
